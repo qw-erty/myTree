dfa446d7d2960b5732e1e7e86d8f47504bd017a6
Mammoth <76951955+mammothneck@users.noreply.github.com>
2021-10-06 22:33:06 +0530

    Add files via upload

---
+++ bloodbank hosted website/bloodbank.github.io/plasmastorage.php
@@ -0,0 +1,23 @@
+<?php
+    
+    
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+    $bloodgroup = $_POST['bloodgroup'];
+    $phone = $_POST['phone'];
+    //database connection
+    $conn = new mysqli( 'localhost','root','','bloodstorage');
+
+
+    if($conn->connect_error){
+        die('connection failed :' .$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into plasmadata(name,age,gender,bloodgroup,phone ) values (?,?,?,?,?)");
+        $stmt->bind_param("sissi",$name,$age, $gender,$bloodgroup, $phone);
+        $stmt->execute();
+        echo "registeration successfully";
+        $stmt->close();
+        $conn->close();
+    }
+?>
